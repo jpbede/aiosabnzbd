b206880854ef24abd3e7443e3170f2b9a79a875f
Jan-Philipp Benecke <jan-philipp@bnck.me>
2024-11-25 19:50:26 +0100

    Add serialization strategy for timeleft field

--- aiosabnzbd/models/queue.py
+++ aiosabnzbd/models/queue.py
@@ -1,10 +1,31 @@
 """Models for the SABnzbd queue API."""
 
 from dataclasses import dataclass, field
+from datetime import UTC, datetime, timedelta
 
+from mashumaro import field_options
 from mashumaro.mixins.orjson import DataClassORJSONMixin
+from mashumaro.types import SerializationStrategy
 
 from aiosabnzbd.const import QueueStatus
+
+
+class HumanReadableAsTimeDelta(SerializationStrategy):
+    """Serialization strategy for timedelta to humanreadable string."""
+
+    def __init__(self, fmt: str) -> None:
+        """Initialize the serialization strategy."""
+        self.fmt = fmt
+
+    def serialize(self, value: timedelta) -> str:
+        """Serialize a timedelta to a humanreadable string."""
+        d = datetime.now(tz=UTC) + value
+        return d.strftime(self.fmt)
+
+    def deserialize(self, value: str) -> timedelta:
+        """Deserialize a humanreadable string to a timedelta."""
+        d = datetime.strptime(value, self.fmt)  # noqa: DTZ007
+        return timedelta(hours=d.hour, minutes=d.minute, seconds=d.second)
 
 
 @dataclass(frozen=True, kw_only=True, slots=True)
@@ -44,7 +65,11 @@ class Queue(DataClassORJSONMixin):
     noofslots: int
     limit: int
     start: int
-    timeleft: str
+    timeleft: timedelta = field(
+        metadata=field_options(
+            serialization_strategy=HumanReadableAsTimeDelta(fmt="%H:%M:%S")
+        )
+    )
     speed: str
     kb_per_sec: str = field(metadata={"alias": "kbpersec"})
     size: str
