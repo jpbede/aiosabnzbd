855d202eae14a785493ea454f5aae39f3db2232b
Jan-Philipp Benecke <jan-philipp@bnck.me>
2024-11-25 19:33:11 +0100

    Refactor request to use one request class (#68)

--- aiosabnzbd/models/__init__.py
+++ aiosabnzbd/models/__init__.py
@@ -1,15 +1,13 @@
 """Models to the SABnzbd API."""
 
 from .base import SabnzbdRequest
-from .queue import Queue, QueueOperationRequest, QueueRequest, QueueResponse, Slot
+from .queue import Queue, QueueResponse, Slot
 from .status import StatusResponse
 
 __all__ = [
     "Queue",
     "Slot",
     "QueueResponse",
-    "QueueRequest",
-    "QueueOperationRequest",
     "StatusResponse",
     "SabnzbdRequest",
 ]
--- aiosabnzbd/models/base.py
+++ aiosabnzbd/models/base.py
@@ -7,7 +7,19 @@ from dataclasses import dataclass
 class SabnzbdRequest:
     """Base request."""
 
+    mode: str | None = None
+    name: str | None = None
+    value: str | int | None = None
+
     @property
     def query_params(self) -> dict[str, str]:
         """Return the query parameters."""
-        return {}
+        params = {}
+        if self.mode is not None:
+            params["mode"] = self.mode
+        if self.name is not None:
+            params["name"] = self.name
+        if self.value is not None:
+            params["value"] = str(self.value)
+
+        return params
